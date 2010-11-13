d4b85407e0f08f71b6db60b4d3ca3f66406ced40
Jonas Baumann <jone@jone.ch>
2010-11-13 12:53:34 +0100

    use ftw-mode in xml mode

--- private/languages/html.el
+++ private/languages/html.el
@@ -9,7 +9,8 @@
 
 ;; Hooks
 (defun default-html-mode-hook ()
-  (set-pairs '("<" "{" "[" "\"" "\'")))
+  (set-pairs '("<" "{" "[" "\"" "\'"))
+  (ftw-mode t))
 
 (add-hook 'html-mode-hook 'default-html-mode-hook)
 (add-hook 'nxml-mode-hook 'default-html-mode-hook)
