59f1f6877b3677b9dcf835f8c5fa25667380dc60
Jonas Baumann <jone@jone.ch>
2010-12-28 13:15:28 +0100

    python: use absolute path for pyflakes, works better

--- private/languages/python.el
+++ private/languages/python.el
@@ -20,7 +20,7 @@
              (local-file (file-relative-name
                           temp-file
                           (file-name-directory buffer-file-name))))
-        (list "pyflakes" (list local-file))))
+        (list (expand-file-name "~/bin/pyflakes") (list local-file))))
 
     (add-to-list 'flymake-allowed-file-name-masks
                  '("\\.py\\'" flymake-pyflakes-init)))
